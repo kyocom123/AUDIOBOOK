23c9996bfde33af8f84ba0f650c28e14e9318a04
kyocom123 <tunm1106@gmail.com>
2013-10-24 23:56:52 -0700

    tao file moi

---
+++ AUDIOBOOK/AUDIOBOOK/Controller/FileMoi2ViewController.h
@@ -0,0 +1,13 @@
+//
+//  FileMoi2ViewController.h
+//  AUDIOBOOK
+//
+//  Created by SALEM CAT on 25/10/2013.
+//  Copyright (c) Năm 2013 SALEM CAT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FileMoi2ViewController : UIViewController
+
+@end
