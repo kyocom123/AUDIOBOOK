bddb97a9d770818107ae256f9fea5cee8c7052d2
kyocom123 <tunm1106@gmail.com>
2013-10-24 21:57:52 -0700

    commit ca file solution thu xem the na0

---
+++ AUDIOBOOK/AUDIOBOOK/Controller/TuDauChay.m
@@ -0,0 +1,38 @@
+//
+//  TuDauChay.m
+//  AUDIOBOOK
+//
+//  Created by SALEM CAT on 25/10/2013.
+//  Copyright (c) Năm 2013 SALEM CAT. All rights reserved.
+//
+
+#import "TuDauChay.h"
+
+@interface TuDauChay ()
+
+@end
+
+@implementation TuDauChay
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
